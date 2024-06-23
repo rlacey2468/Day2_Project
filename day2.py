aa399ce2a7b68318ec019de543f8d4d81f46bd7f
Rachel Lacey <rlacey3@binghamton.edu>
2024-06-23 00:40:22 +0000

    Third Edit

--- day2.py
+++ day2.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import pandas as pd
+import sqlalchemy as db
 from pyyoutube import Api
+
 
 
 API_KEY= "AIzaSyAr57EoQGSk5pW7WnpxblC0Q_7xcHtKXNw"
@@ -23,14 +26,31 @@ if response.status_code == 200:
   for item in data.get('items', []):
         video_id = item['id']
         title = item['snippet']['title']
-        description = item['snippet']['description']
+        #description = item['snippet']['description']
         
         print(f"Video ID: {video_id}")
         print(f"Title: {title}")
-        print(f"Description: {description}")
+        #print(f"Description: {description}")
         print("\n")
 else:
   print(response.status_code)
 
+video_data = []
+for item in data.get('items', []):
+    video_data.append({
+        'video_id': item['id'],
+        'title': item['snippet']['title'],
+        'published_at': item['snippet']['publishedAt'],
+        'channel_title': item['snippet']['channelTitle']
+        # Add more fields as needed
+    })
+print(video_data)
 
+data_base = pd.DataFrame.from_dict(video_data)
+engine = db.create_engine('sqlite:///data_base.db')
 
+data_base.to_sql('videos', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
